891ea3d7b8108aee3b230c81ba00756c2c85d2a2
seidaiegoshi <egoshi.seidai@gmail.com>
2023-03-02 20:23:55 +0900

    設定

--- haku-bill/vite.config.js
+++ haku-bill/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	// 自動的にブラウザ開く
+	server: {
+		open: true,
+	},
+	// パスエイリアスの設定
+	// src以下を@で記載できる
+	resolve: {
+		alias: {
+			"@/": `${__dirname}/src/`,
+		},
+	},
+	plugins: [react()],
+});
