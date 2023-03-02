a604b46bbd18b11d1235f9fd7148e568cf1e7942
seidaiegoshi <egoshi.seidai@gmail.com>
2023-03-02 22:18:31 +0900

    axios

---
+++ haku-bill/src/libs/axios.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { getToken } from "@/utils/auth";
+
+const api = axios.create({
+	baseURL: import.meta.env.VITE_API_ENDPOINT,
+	timeout: 60000,
+});
+
+api.interceptors.request.use((config) => {
+	const token = getToken();
+	if (!!token && !!config.headers) {
+		config.headers.Authorization = `Bearer ${token}`;
+	}
+	return config;
+});
+
+api.interceptors.response.use(
+	(response) => {
+		return response;
+	},
+	(error) => {
+		throw error;
+	}
+);
+
+export default api;
