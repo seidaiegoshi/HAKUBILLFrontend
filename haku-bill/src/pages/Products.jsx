204344602a7a466fbb7c9ddb279e86e6c10f7c4b
seidaiegoshi <egoshi.seidai@gmail.com>
2023-03-03 11:55:20 +0900

    apiテスト

--- haku-bill/src/pages/Products.jsx
+++ haku-bill/src/pages/Products.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import axios from "@/libs/axios";
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
-	return <div>Products{import.meta.env.VITE_API_ENDPOINT}</div>;
+	const [products, setProducts] = useState([]);
+
+	const fetchProducts = async () => {
+		const response = await axios.get("/products");
+		setProducts(response.data);
+		console.log(response.data);
+	};
+
+	useEffect(() => {
+		fetchProducts();
+	}, []);
+
+	return (
+		<div>
+			<p>Products Page</p>
+			{/* {products.map((product) => (
+				<p key={product.id}>{product.body}</p>
+			))} */}
+		</div>
+	);
 };
 
 export default Products;
