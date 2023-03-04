bca4a4177a287784a0824286008e8fbfd3f4a350
seidaiegoshi <egoshi.seidai@gmail.com>
2023-03-05 02:11:01 +0900

    登録してあるデータを表示する

--- haku-bill/src/pages/settings/Products.jsx
+++ haku-bill/src/pages/settings/Products.jsx
@@ -22,9 +22,50 @@ const Products = () => {
 					<Sidebar />
 				</div>
 				<div className="flex-initial">
-					{products.map((value) => (
-						<p key={value.id}>{value.name}</p>
-					))}
+					<div className="flex flex-col">
+						<div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
+							<div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+								<div className="overflow-hidden">
+									<table className="min-w-full">
+										<thead className="bg-white border-b">
+											<tr>
+												<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+													商品名
+												</th>
+												<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+													単位
+												</th>
+												<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+													原価
+												</th>
+												<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+													価格
+												</th>
+											</tr>
+										</thead>
+										<tbody>
+											{products.map((value) => (
+												<tr key={value.id} className="bg-white border-b">
+													<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+														{value.name}
+													</td>
+													<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+														{value.unit}
+													</td>
+													<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+														{Number(value.cost).toLocaleString("jp-JP") + "円"}
+													</td>
+													<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+														{Number(value.price).toLocaleString("jp-JP") + "円"}
+													</td>
+												</tr>
+											))}
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
