e9d00805de1ce9e08ed5accec4eade1692299dde
seidaiegoshi <egoshi.seidai@gmail.com>
2023-03-08 10:32:30 +0900

    候補表示途中

--- haku-bill/src/components/SideButton.jsx
+++ haku-bill/src/components/SideButton.jsx
@@ -4,7 +4,7 @@ const SideButton = () => {
 	return (
 		<nav className="z-20 flex shrink-0 grow-0 justify-around gap-4 border-t border-gray-200 bg-white/50 p-2.5 shadow-lg backdrop-blur-lg fixed top-2/4 -translate-y-2/4 right-6 min-h-[auto] min-w-[64px] flex-col rounded-lg border">
 			<a
-				href="#settings"
+				href="/"
 				className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 ">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -23,7 +23,9 @@ const SideButton = () => {
 				<small className="text-center text-xs font-medium"> 登録 </small>
 			</a>
 
-			<a href="/" className="flex h-16 w-16 flex-col items-center justify-center gap-1 text-fuchsia-900 ">
+			<a
+				href="/"
+				className="flex h-16 w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 ">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					fill="none"
--- haku-bill/src/pages/deliverySlip/CreateDeliverySlip.jsx
+++ haku-bill/src/pages/deliverySlip/CreateDeliverySlip.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./../../components/Header";
 import SideButton from "../../components/SideButton";
+import TextInput from "../../components/Atoms/TextInput";
 
 const CreateDeliverySlip = () => {
+	// inputに入れる値
+	const [text, setText] = useState("");
+	// inputにフォーカスしているかどうか
+	const [isFocus, setIsFocus] = useState(false);
+	const [suggestions, setSuggestions] = useState([]);
+
+	// inputフィールドのonChangeイベント
+	const handleChange = (text) => {
+		// 入力した値をもとにフィルターをかける。
+		// 空の配列を用意
+		let matches = [];
+		// 入力する値が0文字より大きければ処理を行う
+		if (text.length > 0) {
+			matches = options.filter((opt) => {
+				// new RegExp = パターンでテキストを検索するために使用
+				const regex = new RegExp(`${text}`, "gi");
+				return opt.text.match(regex);
+			});
+		}
+		// フィルターをかけた配列をsuggestionsのステートに入れる
+		setSuggestions(matches);
+		setText(text);
+	};
+
+	const options = [
+		{ id: 1, text: "React" },
+		{ id: 2, text: "Ruby on Rails" },
+		{ id: 3, text: "JavaScript" },
+		{ id: 4, text: "TypeScript" },
+		{ id: 5, text: "Go" },
+		{ id: 6, text: "HTML" },
+		{ id: 7, text: "CSS" },
+	];
+
 	return (
 		<>
 			<Header />
@@ -12,36 +47,94 @@ const CreateDeliverySlip = () => {
 					<div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
 						<div className="overflow-hidden">
 							<table className="min-w-full">
-								<thead className="bg-white border-b">
+								<thead className="bg-white border-b-2">
 									<tr>
 										<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
 											#
 										</th>
 										<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-											First
+											商品名
 										</th>
 										<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-											Last
+											単位
 										</th>
 										<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-											Handle
+											金額
+										</th>
+										<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+											数量
+										</th>
+										<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+											小計
 										</th>
 									</tr>
 								</thead>
 								<tbody>
-									<tr className="bg-gray-100 border-b">
+									<tr className="bg-white-100 border-b">
 										<td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
-										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">Mark</td>
+										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+											<TextInput
+												onFocus={() => setIsFocus(true)}
+												type="text"
+												value={text}
+												onChange={(e) => handleChange(e.target.value)}
+											/>
+
+											{isFocus && (
+												<div className="shadow-lg shadow-slate-500/50 bg-white absolute">
+													{suggestions?.map((suggestion, i) => (
+														<p
+															className="p-2"
+															key={i}
+															onClick={async () => {
+																await setText(suggestion.text);
+																await setIsFocus(false);
+															}}>
+															{suggestion.text}
+														</p>
+													))}
+												</div>
+											)}
+										</td>
 										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">Otto</td>
+										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+											<TextInput />
+										</td>
+										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+											<TextInput />
+										</td>
 										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">@mdo</td>
 									</tr>
 									<tr className="bg-white border-b">
 										<td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
-										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">Jacob</td>
+										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+											<TextInput
+												onFocus={() => setIsFocus(true)}
+												type="text"
+												value={text}
+												onChange={(e) => handleChange(e.target.value)}
+											/>
+
+											{isFocus && (
+												<div className="shadow-lg shadow-slate-500/50 bg-white absolute">
+													{suggestions?.map((suggestion, i) => (
+														<p
+															className="p-2"
+															key={i}
+															onClick={async () => {
+																await setText(suggestion.text);
+																await setIsFocus(false);
+															}}>
+															{suggestion.text}
+														</p>
+													))}
+												</div>
+											)}
+										</td>
 										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">Dillan</td>
 										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">@fat</td>
 									</tr>
-									<tr className="bg-gray-100 border-b">
+									<tr className="bg-white-100 border-b">
 										<td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">3</td>
 										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">Mark</td>
 										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">Twen</td>
@@ -52,15 +145,6 @@ const CreateDeliverySlip = () => {
 										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">Bob</td>
 										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">Dillan</td>
 										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">@fat</td>
-									</tr>
-									<tr className="bg-gray-100 border-b">
-										<td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">5</td>
-										<td
-											colSpan="2"
-											className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">
-											Larry the Bird
-										</td>
-										<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">@twitter</td>
 									</tr>
 								</tbody>
 							</table>
