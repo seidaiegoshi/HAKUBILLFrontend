7e98370b984a19e20f832dc8e2c67d4c2a45d422
seidaiegoshi <egoshi.seidai@gmail.com>
2023-03-17 17:23:32 +0900

    利益グラフを表示

--- haku-bill/src/pages/analysis/Analysis.jsx
+++ haku-bill/src/pages/analysis/Analysis.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/Header";
+import axios from "./../../libs/axios.js";
+import { startOfMonth, endOfMonth, format } from "date-fns";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import CalendarComponent from "./CalendarComponent";
 
 const Analysis = () => {
-	const [products, setProducts] = useState([]);
+	const [span, setSpan] = useState(["start_date", "end_date"]);
+	const [graphData, setGraphData] = useState([]);
 
-	const fetchProducts = () => {
-		const requestUrl = "/product";
+	const fetchProfit = (startDate, endDate) => {
+		const requestUrl = `/delivery_slip/daily_profit/${startDate}/${endDate}`;
+		console.log(requestUrl);
 		axios
 			.get(requestUrl)
 			.then((response) => {
-				setProducts(response.data);
+				setGraphData(cumulativeData(response.data));
+				setSpan([response.data[0].publish_date, response.data[response.data.length - 1].publish_date]);
 			})
 			.catch((error) => {
 				console.log(error);
@@ -17,14 +24,55 @@ const Analysis = () => {
 	};
 
 	useEffect(() => {
-		fetchProducts();
+		const now = new Date();
+		const start = format(startOfMonth(now), "yyyy-MM-dd");
+		const end = format(endOfMonth(now), "yyyy-MM-dd");
+		fetchProfit(start, end);
 	}, []);
+
+	const cumulativeData = (data) => {
+		let cumulative = 0;
+		const newData = [];
+		data.forEach((obj) => {
+			cumulative += Number(Math.floor(obj.sum_gross_profit));
+			newData.push({
+				publish_date: obj.publish_date,
+				daily_profit: cumulative,
+			});
+		});
+
+		return newData;
+	};
 
 	return (
 		<>
 			<Header />
-
 			<div>Analysis</div>
+
+			<div>
+				<div>
+					<CalendarComponent getData={fetchProfit} />
+				</div>
+				<div>
+					<ResponsiveContainer width="90%" height={500}>
+						<LineChart
+							data={graphData}
+							margin={{
+								top: 20,
+								right: 30,
+								left: 20,
+								bottom: 5,
+							}}>
+							<CartesianGrid strokeDasharray="3 3" />
+							<XAxis dataKey="publish_date" />
+							<YAxis domain={[0, "dataMax + 1000"]} />
+							<Tooltip />
+							<Legend />
+							<Line type="monotone" dataKey="daily_profit" stroke="#8884d8" />
+						</LineChart>
+					</ResponsiveContainer>
+				</div>
+			</div>
 		</>
 	);
 };
