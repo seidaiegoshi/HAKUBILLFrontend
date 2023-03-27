0105c6f346e646b86e890b6423a62fdf5176387e
seidaiegoshi <egoshi.seidai@gmail.com>
2023-03-27 21:10:08 +0900

    ルーター変更

--- haku-bill/src/router/router.jsx
+++ haku-bill/src/router/router.jsx
@@ -1,17 +1,16 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
-import Home from "../pages/Home";
-import DeliverySlip from "../pages/DeliverySlip";
-import Settings from "../pages/Settings";
-import Products from "../pages/settings/product/Products";
-import ProductAdd from "../pages/settings/product/ProductAdd";
-import Analysis from "../pages/analysis/Analysis";
-import FixedCosts from "../pages/settings/fixed_cost/FixedCosts";
-import FixedCostAdd from "../pages/settings/fixed_cost/FixedCostAdd";
-import ProductCategory from "../pages/settings/category/Category";
-import CategoryAdd from "../pages/settings/category/CategoryAdd";
-import ProductEdit from "../pages/settings/product/ProductEdit";
-import CategoryEdit from "../pages/settings/category/CategoryEdit";
-import FixedCostEdit from "../pages/settings/fixed_cost/FixedCostEdit";
+import Home from "@/pages/Home";
+import Products from "@/pages/settings/product/Products";
+import ProductAdd from "@/pages/settings/product/ProductAdd";
+import Analysis from "@/pages/analysis/Analysis";
+import FixedCosts from "@/pages/settings/fixed_cost/FixedCosts";
+import FixedCostAdd from "@/pages/settings/fixed_cost/FixedCostAdd";
+import ProductCategory from "@/pages/settings/category/Category";
+import CategoryAdd from "@/pages/settings/category/CategoryAdd";
+import ProductEdit from "@/pages/settings/product/ProductEdit";
+import CategoryEdit from "@/pages/settings/category/CategoryEdit";
+import FixedCostEdit from "@/pages/settings/fixed_cost/FixedCostEdit";
+import CreateDeliverySlip from "@/pages/deliverySlip/CreateDeliverySlip";
 
 const createRoutes = (element) => createBrowserRouter(createRoutesFromElements(element));
 
@@ -19,10 +18,9 @@ const routes = createRoutes(
 	<Route path="/" errorElement={<div>error</div>}>
 		<Route index element={<Home />} />
 
-		<Route path="/delivery-slip" element={<DeliverySlip />} />
+		<Route path="/delivery-slip" element={<CreateDeliverySlip />} />
 
 		<Route path="/analysis" element={<Analysis />} />
-		<Route path="/setting" element={<Settings />} />
 		<Route path="/setting/product" element={<Products />} />
 		<Route path="/setting/product/new" element={<ProductAdd />} />
 		<Route path="/setting/product/:id/edit" element={<ProductEdit />} />
