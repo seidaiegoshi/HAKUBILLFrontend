f9bf51d58b3ed6253662c1df881dd530ee1ebeb0
seidaiegoshi <egoshi.seidai@gmail.com>
2023-03-28 23:33:36 +0900

    コメントアウトしとく

--- haku-bill/src/components/Header.jsx
+++ haku-bill/src/components/Header.jsx
@@ -20,8 +20,8 @@ const Header = () => {
 					<span className="ml-3 text-xl">HAKU Bill</span>
 				</a>
 				<nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-					<a className="mr-5 hover:text-gray-900">製造</a>
-					<a className="mr-5 hover:text-gray-900">在庫</a>
+					{/* <a className="mr-5 hover:text-gray-900">製造</a> */}
+					{/* <a className="mr-5 hover:text-gray-900">在庫</a> */}
 					<Link to="/delivery-slip/new" className="mr-5 hover:text-gray-900">
 						納品
 					</Link>
