6409f887b44e4d73fafdcf8b5683bf5219a0da21
seidaiegoshi <egoshi.seidai@gmail.com>
2023-03-28 21:24:54 +0900

    見た目を少し変更

--- haku-bill/src/pages/analysis/ProductSales.jsx
+++ haku-bill/src/pages/analysis/ProductSales.jsx
@@ -22,7 +22,7 @@ const ProductSales = () => {
 	const [sortOrder, setSortOrder] = useState("desc");
 	const [sortColumn, setSortColumn] = useState("created_at");
 
-	const chartHeight = salesData.length * 50;
+	const chartHeight = 50 + salesData.length * 50;
 
 	const addPercentage = (data) => {
 		const totalProfit = data.reduce((total, current) => total + Number(current.sum_gross_profit), 0);
@@ -86,107 +86,109 @@ const ProductSales = () => {
 					<AnalysisSidebar />
 				</div>
 				<div className="flex-initial w-full">
-					<div>Product Sales</div>
-					<div>
+					<div className="min-h-screen">
+						<div>Product Sales</div>
 						<div>
 							<CalendarComponent getData={fetchProfit} />
 						</div>
-						<div>
-							<table className="min-w-full">
-								<thead className="bg-white border-b sticky top-0">
-									<tr>
-										<th
-											scope="col"
-											className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-											onClick={() => handleSortClick("name")}>
-											商品名
-											{sortColumn === "name" && (
-												<span className="ml-2">{sortOrder === "asc" ? "↑" : "↓"}</span>
-											)}
-										</th>
-										<th
-											scope="col"
-											className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-											onClick={() => handleSortClick("sum_gross_profit")}>
-											粗利
-											{sortColumn === "sum_gross_profit" && (
-												<span className="ml-2">{sortOrder === "asc" ? "↑" : "↓"}</span>
-											)}
-										</th>
-										<th
-											scope="col"
-											className="text-sm font-medium text-gray-900 px-6 py-2 text-left"
-											onClick={() => handleSortClick("profit_rate")}>
-											粗利割合
-											{sortColumn === "profit_rate" && (
-												<span className="ml-2">{sortOrder === "asc" ? "↑" : "↓"}</span>
-											)}
-										</th>
-										<th
-											scope="col"
-											className="text-sm font-medium text-gray-900 px-6 py-2 text-left"
-											onClick={() => handleSortClick("sum_quantity")}>
-											販売数
-											{sortColumn === "sum_quantity" && (
-												<span className="ml-2">{sortOrder === "asc" ? "↑" : "↓"}</span>
-											)}
-										</th>
-										<th
-											scope="col"
-											className="text-sm font-medium text-gray-900 px-6 py-2 text-left"
-											onClick={() => handleSortClick("quantity_rate")}>
-											販売数割合
-											{sortColumn === "quantity_rate" && (
-												<span className="ml-2">{sortOrder === "asc" ? "↑" : "↓"}</span>
-											)}
-										</th>
-									</tr>
-								</thead>
-								<tbody>
-									{sortedSalesData.map((value) => (
-										<tr key={value.product_id} className="bg-white border-b">
-											<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-												{value.name}
-											</td>
-											<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-												{Number(value.sum_gross_profit).toLocaleString("jp-JP") + "円"}
-											</td>
-											<td className="text-sm text-gray-900 font-light px-6 py-2 whitespace-nowrap">
-												{value.profit_rate.toFixed(2)}%
-											</td>
-											<td className="text-sm text-gray-900 font-light px-6 py-2 whitespace-nowrap">
-												{value.sum_quantity + " " + value.unit}
-											</td>
-											<td className="text-sm text-gray-900 font-light px-6 py-2 whitespace-nowrap">
-												{value.quantity_rate.toFixed(2)}%
-											</td>
+						<div className="flex flex-row">
+							<div className="w-1/2">
+								<table className="min-w-full">
+									<thead className="bg-white border-b sticky top-0">
+										<tr>
+											<th
+												scope="col"
+												className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+												onClick={() => handleSortClick("name")}>
+												商品名
+												{sortColumn === "name" && (
+													<span className="ml-2">{sortOrder === "asc" ? "↑" : "↓"}</span>
+												)}
+											</th>
+											<th
+												scope="col"
+												className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+												onClick={() => handleSortClick("sum_gross_profit")}>
+												粗利
+												{sortColumn === "sum_gross_profit" && (
+													<span className="ml-2">{sortOrder === "asc" ? "↑" : "↓"}</span>
+												)}
+											</th>
+											<th
+												scope="col"
+												className="text-sm font-medium text-gray-900 px-6 py-2 text-left"
+												onClick={() => handleSortClick("profit_rate")}>
+												粗利割合
+												{sortColumn === "profit_rate" && (
+													<span className="ml-2">{sortOrder === "asc" ? "↑" : "↓"}</span>
+												)}
+											</th>
+											<th
+												scope="col"
+												className="text-sm font-medium text-gray-900 px-6 py-2 text-left"
+												onClick={() => handleSortClick("sum_quantity")}>
+												販売数
+												{sortColumn === "sum_quantity" && (
+													<span className="ml-2">{sortOrder === "asc" ? "↑" : "↓"}</span>
+												)}
+											</th>
+											<th
+												scope="col"
+												className="text-sm font-medium text-gray-900 px-6 py-2 text-left"
+												onClick={() => handleSortClick("quantity_rate")}>
+												販売数割合
+												{sortColumn === "quantity_rate" && (
+													<span className="ml-2">{sortOrder === "asc" ? "↑" : "↓"}</span>
+												)}
+											</th>
 										</tr>
-									))}
-								</tbody>
-							</table>
-						</div>
-						<div>
-							<ResponsiveContainer width="100%" height={chartHeight}>
-								<BarChart
-									width={600}
-									height={300}
-									data={sortedSalesData}
-									layout="vertical"
-									margin={{
-										top: 20,
-										right: 50,
-										left: 100,
-										bottom: 5,
-									}}>
-									<CartesianGrid strokeDasharray="3 3" />
-									<XAxis type="number" domain={[0, "auto"]} />
-									<YAxis dataKey="name" type="category" />
-									<Tooltip />
-									<Legend verticalAlign="top" height={36} />
-									<Bar dataKey="profit_rate" name="総粗利" fill="#82ca9d" barSize={10} />
-									<Bar dataKey="quantity_rate" name="販売数" fill="#8884d8" barSize={10} />
-								</BarChart>
-							</ResponsiveContainer>
+									</thead>
+									<tbody>
+										{sortedSalesData.map((value) => (
+											<tr key={value.product_id} className="bg-white border-b">
+												<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+													{value.name}
+												</td>
+												<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+													{Number(value.sum_gross_profit).toLocaleString("jp-JP") + "円"}
+												</td>
+												<td className="text-sm text-gray-900 font-light px-6 py-2 whitespace-nowrap">
+													{value.profit_rate.toFixed(2)}%
+												</td>
+												<td className="text-sm text-gray-900 font-light px-6 py-2 whitespace-nowrap">
+													{value.sum_quantity + " " + value.unit}
+												</td>
+												<td className="text-sm text-gray-900 font-light px-6 py-2 whitespace-nowrap">
+													{value.quantity_rate.toFixed(2)}%
+												</td>
+											</tr>
+										))}
+									</tbody>
+								</table>
+							</div>
+							<div className="w-1/2">
+								<ResponsiveContainer width="100%" height={chartHeight}>
+									<BarChart
+										width={600}
+										height={300}
+										data={sortedSalesData}
+										layout="vertical"
+										margin={{
+											top: 20,
+											right: 50,
+											left: 100,
+											bottom: 5,
+										}}>
+										<CartesianGrid strokeDasharray="3 3" />
+										<XAxis type="number" domain={[0, "auto"]} />
+										<YAxis dataKey="name" type="category" />
+										<Tooltip />
+										<Legend verticalAlign="top" height={36} />
+										<Bar dataKey="profit_rate" name="総粗利" fill="#82ca9d" barSize={10} />
+										<Bar dataKey="quantity_rate" name="販売数" fill="#8884d8" barSize={10} />
+									</BarChart>
+								</ResponsiveContainer>
+							</div>
 						</div>
 					</div>
 				</div>
